485f2761a2b493eaed807a9dd3907e9a011025e1
qhz1220 <120169976+qhz1220@users.noreply.github.com>
2024-04-18 15:42:00 +0800

    Update main.js

--- landeye/src/main.js
+++ landeye/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import http from './http'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
